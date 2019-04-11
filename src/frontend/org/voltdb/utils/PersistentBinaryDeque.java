97b266f8c795d6fad842b7c091c31678f823eca3
Nate Clark <nclark@voltdb.com>
2019-04-11 09:00:14 -0400

    PersistentBinaryDeque: Remove awaitingTruncation
    The awaitingTruncation concept is really a DR concept and should be implemented
    in the DR code and not in the common PBD code.

--- src/frontend/org/voltdb/utils/PersistentBinaryDeque.java
+++ src/frontend/org/voltdb/utils/PersistentBinaryDeque.java
@@ -345,7 +345,6 @@ public class PersistentBinaryDeque implements BinaryDeque {
     private final String m_nonce;
     private final boolean m_compress;
     private boolean m_initializedFromExistingFiles = false;
-    private boolean m_awaitingTruncation = false;
 
     //Segments that are no longer being written to and can be polled
     //These segments are "immutable". They will not be modified until deletion
@@ -1251,16 +1250,6 @@ public class PersistentBinaryDeque implements BinaryDeque {
         }
 
         return numOpen;
-    }
-
-    public synchronized boolean isAwaitingTruncation()
-    {
-        return m_awaitingTruncation;
-    }
-
-    public synchronized void setAwaitingTruncation(boolean m_awaitingTruncation)
-    {
-        this.m_awaitingTruncation = m_awaitingTruncation;
     }
 
     @Override
